f624a2d05d35a2bebf4a3f2fbb140c6dbcdcf6c0
Maruf <66161467+marufpbt@users.noreply.github.com>
2021-01-27 16:48:49 +0600

    first commit

---
+++ style.js
@@ -0,0 +1,10 @@
+	//add plus
+	var count = 1;
+	var getSpinnerText = document.getElementById("getSpinnerText");
+	document.getElementById("addPlus").addEventListener("click",function(){
+		count++;
+		getSpinnerText.value = count;
+
+	});
+
+	//add minus
