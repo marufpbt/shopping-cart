88233d46cc34804cdc67754a82fe38a0c29578cb
Maruf <66161467+marufpbt@users.noreply.github.com>
2021-01-29 01:04:19 +0600

    case & phone both with merginng

--- style.js
+++ style.js
@@ -1,52 +1,42 @@
 //case increase + case decrease with merging
 document.getElementById("caseIncrease").addEventListener("click", function () {
-	handleProductChange(true);
+	handleProductChange('case', true);
 })
 
 
-document.getElementById("caseDecrease").addEventListener("click",function(){
-	handleProductChange(false);
+document.getElementById("caseDecrease").addEventListener("click", function () {
+	handleProductChange('case', false);
 })
-
-
-function handleProductChange(isIncrease){
-	const caseInput = document.getElementById("caseInput");
-	const caseCount = parseInt(caseInput.value);
-	let caseNewCount = caseCount;
-	if (isIncrease == true){
-		caseNewCount = caseCount + 1;
-	}
-	if (isIncrease == false && caseCount > 0) {
-		caseNewCount = caseCount - 1;
-	}
-	caseInput.value = caseNewCount;
-	const caseTotal = document.getElementById("caseTotal");
-	caseTotal.innerText = caseNewCount * 59;
-}
 
 //phone increase + phone decrease with merging
 document.getElementById("phoneIncrease").addEventListener("click", function () {
-	handlePhoneChange(true);
+	handleProductChange('phone', true);
 })
 
 
-document.getElementById("phoneDecrease").addEventListener("click",function(){
-	handlePhoneChange(false);
+document.getElementById("phoneDecrease").addEventListener("click", function () {
+	handleProductChange('phone', false);
 })
 
+function handleProductChange(product, isIncrease) {
+	const productInput = document.getElementById(product + 'Input');
+	const productCount = parseInt(productInput.value);
+	let productNewCount = productCount;
+	if (isIncrease == true) {
+		productNewCount = productCount + 1;
+	}
+	if (isIncrease == false && productCount > 0) {
+		productNewCount = productCount - 1;
+	}
+	productInput.value = productNewCount;
+	productTotal = 0;
+	if (product == 'phone') {
+		productTotal = productNewCount * 1229;
+	}
+	if (product == 'case') {
+		productTotal = productNewCount * 59;
+	}
+	document.getElementById(product + 'Total').innerText = productTotal;
 
-function handlePhoneChange(isIncrease){
-	const phoneInput = document.getElementById("phoneInput");
-	const phoneCount = parseInt(phoneInput.value);
-	let phoneNewCount = phoneCount;
-	if (isIncrease == true){
-		phoneNewCount = phoneCount + 1;
-	}
-	if (isIncrease == false && phoneCount > 0) {
-		phoneNewCount = phoneCount - 1;
-	}
-	phoneInput.value = phoneNewCount;
-	const phoneTotal = document.getElementById("phoneTotal");
-	phoneTotal.innerText = phoneNewCount * 1229;
 }
 
